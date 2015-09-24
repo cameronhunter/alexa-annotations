d6f5c258a0209f668f8ba9af768fc60e100103a3
Cameron Hunter <chunter@twitter.com>
2015-09-24 12:00:33 -0700

    Separate alexa into smaller modules

---
+++ src/alexa/handler.js
@@ -0,0 +1,17 @@
+import Router, { NotFound } from "./router";
+
+export default function Handler(skill, router = Router) {
+  return function handle(event, context) {
+    const { request = {} } = event;
+    const { intent: { slots = {} } = {} } = request;
+    const { handler = NotFound } = router(request, skill);
+    const intentData = Object.values(slots).reduce((result, { name, value }) => Object.assign({}, result, { [name]: value }), {});
+
+    Promise.resolve(handler(intentData)).then(response => {
+      const data = response ? { version: "1.0", sessionAttributes: {}, response: { ...response } } : undefined;
+      context.succeed(data);
+    }).catch(error => {
+      context.fail(error || "Unknown error");
+    });
+  }
+}
--- src/alexa/index.js
+++ src/alexa/index.js
@@ -1,33 +1,4 @@
-const NotFound = () => Promise.reject("404 Not Found");
-
-function Router(request, skill) {
-  const { type, intent: { name } = {} } = request;
-  return [
-    type == "IntentRequest" && name ? name : null,
-    type == "IntentRequest" ? "intent" : null,
-    type == "LaunchRequest" ? "launch" : null,
-    type == "SessionEndedRequest" ? "sessionEnded" : null,
-    "notFound"
-  ].filter(name => name != null)
-   .map(name => ({ name: name, handler: skill[name] }))
-   .filter(({ handler }) => handler != null)[0];
-}
-
-function Handler(skill, router = Router) {
-  return function handle(event, context) {
-    const { request = {} } = event;
-    const { intent: { slots = {} } = {} } = request;
-    const { handler = NotFound } = router(request, skill);
-    const intentData = Object.values(slots).reduce((result, { name, value }) => Object.assign({}, result, { [name]: value }), {});
-
-    Promise.resolve(handler(intentData)).then(response => {
-      const data = response ? { version: "1.0", sessionAttributes: {}, response: { ...response } } : undefined;
-      context.succeed(data);
-    }).catch(error => {
-      context.fail(error || "Unknown error");
-    });
-  }
-}
+import Handler from "./handler";
 
 export default {
   Handler: Handler
